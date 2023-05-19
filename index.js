f0f34207d3b831223e5e60b8b098c2917d09561d
Jean Carlos <jeancapedrozo@gmail.com>
2023-05-19 00:47:56 -0500

    configure express and environment variables

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("hola!! =)");
+const express = require("express");
+require("dotenv").config();
+
+const app = express();
+
+app.use(express.static("public"));
+
+// app.get("/", (req, res) => {
+//   res.json({
+//     ok: true,
+//   });
+// });
+
+app.listen(process.env.PORT, () => {
+  console.log(`server corriendo en el puerto ${process.env.PORT}`);
+});
